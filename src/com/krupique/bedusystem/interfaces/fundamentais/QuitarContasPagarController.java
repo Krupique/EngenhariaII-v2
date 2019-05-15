64c205da4cc72261eb100e5ee0c5604ed367fe0e
Henrique Krupck Secchi <krupique@outlook.com>
2019-05-15 18:04:36 -0300

    Comecei a tela de Quitar contas à pagar.

--- src/com/krupique/bedusystem/interfaces/fundamentais/QuitarContasPagarController.java
+++ src/com/krupique/bedusystem/interfaces/fundamentais/QuitarContasPagarController.java
@@ -5,11 +5,14 @@
  */
 package com.krupique.bedusystem.interfaces.fundamentais;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
@@ -20,6 +23,18 @@ public class QuitarContasPagarController implements Initializable {
 
     @FXML
     private BorderPane paneprincipal;
+    @FXML
+    private HBox paneBotoes;
+    @FXML
+    private JFXButton btPagar;
+    @FXML
+    private JFXButton brRemover;
+    @FXML
+    private JFXButton btCancelar;
+    @FXML
+    private JFXButton btBuscar;
+    @FXML
+    private JFXButton btVoltar;
 
     /**
      * Initializes the controller class.
@@ -28,5 +43,25 @@ public class QuitarContasPagarController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void evtPagar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtRemover(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtVoltar(ActionEvent event) {
+    }
     
 }
